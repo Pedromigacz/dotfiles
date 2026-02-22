319c88c9323d8058eb83e0853dd2d754b014052e
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-22 20:20:40 -0300

    feat(omarchy): add stow all script

--- omarchy-setup/all.sh
+++ omarchy-setup/all.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 "$SCRIPT_DIR/to-install.sh"
 "$SCRIPT_DIR/to-remove.sh"
+"$SCRIPT_DIR/to-stow.sh"
---
+++ omarchy-setup/to-stow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+REPO_ROOT="$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")"
+STOW_FILE="$(dirname "${BASH_SOURCE[0]}")/to-stow"
+
+is_stow_installed() {
+  pacman -Qi "stow" &> /dev/null
+}
+
+if ! is_stow_installed; then
+  echo "Install stow first"
+  exit 1
+fi
+
+while IFS= read -r line; do
+    [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
+
+    if ! stow --restow --adopt -d "$REPO_ROOT" -t "$HOME" "$line"; then
+        echo "Failed to stow $line 😞"
+    else
+        echo "$line stowed successfully 🤸‍♂"
+    fi
+done < "$STOW_FILE"
