dd84bdcfc18281acf76d6eb180f12dd95a10b2ab
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-22 18:42:30 -0300

    feat(omarchy): add mass installation script

--- omarchy-setup/install-all.sh
+++ omarchy-setup/install-all.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# ./install-ghostty.sh
-./install-stow.sh
-./install-dotfiles.sh
-./install-zsh.sh
-# ./install-tmux.sh
+PACKAGES_FILE="$(dirname "$0")/to-install.packages"
 
-# ./set-shell.sh
+while IFS= read -r line; do
+    # Skip comments and empty lines
+    [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
 
+    package="$line"
+
+    if ! yay -Qi "$package" &>/dev/null; then
+        if ! yay -S --noconfirm --needed "$package" 2>/tmp/yay_err >/dev/null; then
+            cat /tmp/yay_err >&2
+            echo "Failed to install $package ❌"
+        else
+            echo "$package installed successfully 🤸‍♂"
+        fi
+    else
+        echo "$package is already installed 🤖"
+    fi
+done < "$PACKAGES_FILE"
