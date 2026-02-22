b6f9d5a00c1adf90b71106b5f706912ea87dc96b
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-22 19:14:02 -0300

    chore(omarchy): remove deprecated scripts

--- omarchy-setup/install-ghostty.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-omarchy-pkg-add ghostty || echo "Erro ao installar ghostty 😞"
-echo "ghostty installed successfully 🤸‍♂"
--- omarchy-setup/install-stow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install stow
-omarchy-pkg-add stow || echo "Erro ao installar stow 😞"
-echo "stow installed successfully 🤸‍♂"
-
