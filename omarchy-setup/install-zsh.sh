2d6369dca92a17f2f263af4ffc28bb46e45f8a35
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-22 20:11:11 -0300

    chore(omarchy): remove deprecated install.zsh script

--- omarchy-setup/install-zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Zsh
-if ! command -v zsh &>/dev/null; then
-    yay -S --noconfirm --needed zsh
-    echo "Zsh installed successfully 🤸‍♂"
-else
-    echo "Zsh is already installed 🤖"
-fi
