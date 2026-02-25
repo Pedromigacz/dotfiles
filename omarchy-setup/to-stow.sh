8210f4ff0bcc78c053f99a9b61dfcbec748b9ef7
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-24 21:32:22 -0300

    fix(omarchy): update stow command

--- omarchy-setup/to-stow.sh
+++ omarchy-setup/to-stow.sh
@@ -15,7 +15,7 @@ fi
 while IFS= read -r line; do
     [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
 
-    if ! stow --restow --adopt -d "$REPO_ROOT" -t "$HOME" "$line"; then
+    if ! stow --restow -d "$REPO_ROOT" -t "$HOME" "$line"; then
         echo "Failed to stow $line 😞"
     else
         echo "$line stowed successfully 🤸‍♂"
