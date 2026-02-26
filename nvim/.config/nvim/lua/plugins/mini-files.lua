569b1e23f43c96fd50162302aa58df5e9dd0b9af
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-25 22:08:30 -0300

    feat(nvim): add minifiles

--- nvim/.config/nvim/lua/plugins/mini-files.lua
+++ nvim/.config/nvim/lua/plugins/mini-files.lua
@@ -1,3 +1,12 @@
 return {
-  { 'nvim-mini/mini.nvim', version = false },
+  {
+    'nvim-mini/mini.nvim',
+    config = function()
+      require('mini.files').setup()
+
+      vim.keymap.set("n", "<leader>e", function()
+        require("mini.files").open()
+      end, { desc = "Open mini.files" })
+    end,
+  },
 }
