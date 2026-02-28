476751ab165fbbf63d5cc61703fc6376005c681a
Pedro H G Migacz <pedromigacz@gmail.com>
2026-02-28 18:45:58 -0300

    feat(nvim): add blink config

---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    opts = {
+      keymap = { preset = 'default' },
+      appearance = {
+        nerd_font_variant = 'mono',
+        use_nvim_cmp_as_default = true,
+      },
+      completion = { documentation = { auto_show = false } },
+      signature = { enabled = true },
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+  }
+}
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -5,10 +5,18 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
+    },
+    opts = {
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({
+            capabilities = require('blink.cmp').get_lsp_capabilities(),
+          })
+        end,
+      },
     },
   }
 }
