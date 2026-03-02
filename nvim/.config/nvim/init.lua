51b440aea5097ee0f6229b2269cac3b572b2129f
Pedro H G Migacz <pedromigacz@gmail.com>
2026-03-01 21:02:50 -0300

    feat(nvim): add relative line numbers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
-
+vim.opt.number = true          -- shows absolute line number on current line
+vim.opt.relativenumber = true  -- shows relative numbers on all other lines
