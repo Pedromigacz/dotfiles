bd581e29208cb99e93dafb58c7815115bbff30cc
Pedro H G Migacz <pedromigacz@gmail.com>
2026-03-04 17:53:41 -0300

    feat(omarch): add firefox pwa bin files

--- omarchy-setup/pre-stow.sh
+++ omarchy-setup/pre-stow.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
 rm -rf ~/.config/nvim
+# Patch omarchy things I don't like
+"$SCRIPT_DIR/patch-omarchy/fix-omarchy-nvim-themes.sh"
+"$SCRIPT_DIR/patch-omarchy/patch-webapp.sh"
