1c00b7fa992b5b691d2f9448d4b9253d87dcac85
Pedro H G Migacz <pedromigacz@gmail.com>
2026-03-05 22:59:10 -0300

    chore(firefox): update pwa styles

--- firefox/.mozilla/firefox/profile.pwas/user.js
+++ firefox/.mozilla/firefox/profile.pwas/user.js
@@ -198,6 +198,8 @@ user_pref("layout.word_select.eat_space_to_next_word", false);
 // visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
 // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
 // Enter your personal overrides below this line:
+user_pref("devtools.selfxss.count", 100);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
 
 
 /****************************************************************************
