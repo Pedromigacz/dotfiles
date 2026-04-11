8d440ac5797251def544155ec2478a47418b636a
Pedro H G Migacz <pedromigacz@gmail.com>
2026-04-11 18:34:23 -0300

    feat(nvim): imrpove folds configs

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true         -- shows absolute line number on current line
+vim.opt.relativenumber = true -- shows relative numbers on all other lines
+
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
