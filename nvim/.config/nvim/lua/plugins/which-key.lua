76ac533f799de80e4450044bab3c1989d6aec5ec
Pedro H G Migacz <pedromigacz@gmail.com>
2026-04-11 18:46:11 -0300

    feat(nvim): add delay and improve layout on whichkey

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -3,10 +3,18 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+      delay = 800,
       plugins = {
         presets = {
           motions = true
         }
+      },
+      win = {
+        no_overlap = false,
+        col = vim.o.columns - 42, -- offset from left = total cols minus popup width
+        row = vim.o.lines - 4,
+        width = 40,
+        border = "rounded",
       },
     },
     keys = {
@@ -18,5 +26,5 @@ return {
         desc = "Buffer Local Keymaps (which-key)",
       },
     },
-  }
+  },
 }
