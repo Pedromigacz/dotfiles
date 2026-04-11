4de1ad7a92a407ababe21867ac05fb6014856fa7
Pedro H G Migacz <pedromigacz@gmail.com>
2026-04-11 19:21:43 -0300

    feat(nvim): open nvim on the same state it was closed

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,21 @@
 require("config.lazy")
 require("config.options")
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Save session on exit
+autocmd("VimLeavePre", {
+  callback = function()
+    vim.cmd("mksession! .session.vim")
+  end,
+})
+
+-- Restore session if one exists in the current directory
+autocmd("VimEnter", {
+  nested = true,
+  callback = function()
+    if vim.fn.argc() == 0 and vim.fn.filereadable(".session.vim") == 1 then
+      vim.cmd("source .session.vim")
+    end
+  end,
+})
