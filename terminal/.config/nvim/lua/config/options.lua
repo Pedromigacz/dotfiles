ce3feaee31d7e105016a2f99fdd0cb82a02842a1
Pedro H G Migacz <pedromigacz@gmail.com>
2026-04-16 21:32:41 -0300

    feat(nvim): add opencode compatibility config

--- terminal/.config/nvim/lua/config/options.lua
+++ terminal/.config/nvim/lua/config/options.lua
@@ -5,3 +5,11 @@ vim.opt.relativenumber = true -- shows relative numbers on all other lines
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- opencode related
+vim.o.autoread = true
+vim.o.updatetime = 2000
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "if mode() !~ '\\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+})
